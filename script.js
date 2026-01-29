3ef924448b5891bec006d405d534dfcfe7f7f147
Reshma Patil <157274743+reshma255@users.noreply.github.com>
2026-01-29 20:29:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,34 +1,21 @@
-//your JS code here. If required.
 const bands = [
-  'The Plot in You',
-  'The Devil Wears Prada',
-  'Pierce the Veil',
-  'Norma Jean',
-  'The Bled',
-  'Say Anything',
-  'The Midway State',
-  'We Came as Romans',
-  'Counterparts',
-  'Oh, Sleeper',
-  'A Skylit Drive',
-  'Anywhere But Here',
-  'An Old Dog'
+  "Anywhere But Here",
+  "The Bled",
+  "Counterparts",
+  "The Devil Wears Prada",
+  "The Midway State",
+  "Norma Jean",
+  "Oh, Sleeper",
+  "An Old Dog",
+  "Pierce the Veil",
+  "The Plot in You",
+  "Say Anything"
 ];
 
+const listElement = document.getElementById("band-list");
 
-function stripArticle(name) {
-  return name.replace(/^(a |an |the )/i, '').trim();
-}
-
-
-const sortedBands = bands.sort((a, b) => {
-  return stripArticle(a).localeCompare(stripArticle(b));
-});
-
-
-const bandList = document.getElementById('band');
-sortedBands.forEach(band => {
-  const li = document.createElement('li');
+bands.forEach(band => {
+  const li = document.createElement("li");
   li.textContent = band;
-  bandList.appendChild(li);
+  listElement.appendChild(li);
 });
